bce653334ec22f9419b41ccd14e2e1804d919a62
libanoca <libanoca@coventry.ac.uk>
2019-10-24 22:43:46 +0100

    put docstrings in all functions to explain what they do

--- cocktail.py
+++ cocktail.py
@@ -15,24 +15,36 @@ headers = {
 
 
 def searchByIngredient(ingredient):
+    '''searchs cocktailAPI for cocktails with the argument ingredient.
+
+    ingredient: string
+    '''
     querystring = {"i":ingredient}
     response = requests.request("GET", url, headers=headers, params=querystring)
     results = json.loads(response.text)
     for i in range(len(results["drinks"])):
         print(results['drinks'][i]['strDrink'])
          
-#searchByIngredient('gin')
+
 
 def listIngredients():
+    '''searchs cocktailAPI for the list of all ingredients available'''
+    
     url = 'https://the-cocktail-db.p.rapidapi.com/list.php?i=list'
     querystring = {"i":'list'}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
     results = json.loads(response.text)
     for i in range(len(results['drinks'])):
        print(results['drinks'][i]['strIngredient1'])
+      
        
+
 def searchByName(name):
+    '''searchs cocktailAPI for cocktails by the cocktail name using the argument name 
+    
+    name: string
+    '''
+    
     url = "https://the-cocktail-db.p.rapidapi.com/search.php"
     querystring = {"s":name}
     response = requests.request("GET", url, headers=headers, params=querystring)
