458a93a2b33257583774bc7a8bb8fca5315d0dcf
libanoca <libanoca@coventry.ac.uk>
2019-10-26 19:19:36 +0100

    I completed the code and fixed some errors

--- cocktail.py
+++ cocktail.py
@@ -12,8 +12,6 @@ headers = {
     }
 
 
-
-
 def searchByIngredient(ingredient):
     '''searchs cocktailAPI for cocktails with the argument ingredient.
 
@@ -22,8 +20,11 @@ def searchByIngredient(ingredient):
     querystring = {"i":ingredient}
     response = requests.request("GET", url, headers=headers, params=querystring)
     results = json.loads(response.text)
-    for i in range(len(results["drinks"])):
-        print(results['drinks'][i]['strDrink'])
+    for i in range(len(results['drinks'])):
+       print('Cocktails: ',results['drinks'][i]['strDrink'],'\n')
+            
+        
+          
          
 
 
@@ -35,7 +36,9 @@ def listIngredients():
     response = requests.request("GET", url, headers=headers, params=querystring)
     results = json.loads(response.text)
     for i in range(len(results['drinks'])):
-       print(results['drinks'][i]['strIngredient1'])
+       print('All Ingredients: ',results['drinks'][i]['strIngredient1'],'\n')
+     
+       
       
        
 
@@ -50,5 +53,54 @@ def searchByName(name):
     response = requests.request("GET", url, headers=headers, params=querystring)
     results = json.loads(response.text)
     for i in range(len(results["drinks"])):
-       print(results['drinks'][i]['strDrink'])
+        print("Name: ",results['drinks'][i]['strDrink'],'\n')
+    for i in range(len(results["drinks"])):
+        print("filter: ",results['drinks'][i]['strAlcoholic'],'\n')
+    for a in range(len(results['drinks'])):
+        print('Instructions: ',results['drinks'][a]['strInstructions'],'\n')
+    for u in range(len(results['drinks'])):
+        print('Ingredients: ','\n')
+        print(results['drinks'][u]['strIngredient1'])  
+        print(results['drinks'][u]['strIngredient2'])
+        print(results['drinks'][u]['strIngredient3']) 
+        print(results['drinks'][u]['strIngredient4']) 
+        print(results['drinks'][u]['strIngredient5'])
+        print(results['drinks'][u]['strIngredient6'])
+        print(results['drinks'][u]['strIngredient7'])
+        print(results['drinks'][u]['strIngredient8'])
+        print(results['drinks'][u]['strIngredient9'])
+    for o in range(len(results['drinks'])):
+        print('image: ',results['drinks'][o]['strDrinkThumb'],'\n')    
+
+
+def randomCocktail():
+    '''searchs CocktailAPI for a random cocktail'''
+    
+    url = "https://the-cocktail-db.p.rapidapi.com/random.php"
+    response = requests.request("GET", url, headers=headers)
+    results = json.loads(response.text)
+    for p in range(len(results['drinks'])):
+        print('name: ',results['drinks'][p]['strDrink'],'\n')
+    for i in range(len(results["drinks"])):
+        print("filter: ",results['drinks'][i]['strAlcoholic'],'\n')
+    for a in range(len(results['drinks'])):
+        print('Instructions: ',results['drinks'][a]['strInstructions'],'\n')
+    for u in range(len(results['drinks'])):
+        print('Ingredients: ','\n')
+        print(results['drinks'][u]['strIngredient1'])  
+        print(results['drinks'][u]['strIngredient2'])
+        print(results['drinks'][u]['strIngredient3']) 
+        print(results['drinks'][u]['strIngredient4']) 
+        print(results['drinks'][u]['strIngredient5'])
+        print(results['drinks'][u]['strIngredient6'])
+        print(results['drinks'][u]['strIngredient7'])
+        print(results['drinks'][u]['strIngredient8'])
+        print(results['drinks'][u]['strIngredient9'])
+    for o in range(len(results['drinks'])):
+        print('image: ',results['drinks'][o]['strDrinkThumb'],'\n' )    
+randomCocktail()      
+
+    
+
+
 
