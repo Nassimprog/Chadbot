1b5cce10e90ef67505beca3457dddb5edbac1df0
Luke Bovington (bovingtl) <bovingtl@coventry.ac.uk>
2019-11-13 11:19:50 +0000

    Add files via upload
    This is all of the code i created for the chat bot chef.

---
+++ ChatBot - Luke/food - luke.py
@@ -0,0 +1,34 @@
+import discord
+import random
+from discord.ext import commands
+import requests
+import asyncio
+import random
+
+client = commands.Bot(command_prefix = '') 
+
+
+
+@client.event
+async def on_ready():
+    print('Chef Chad has awoken')
+
+@client.event
+async def on_message(message):
+    if message.content == 'chicken':        #<---If statement so when the user picks 'chicken' from list, it responds 
+        await message.channel.send('why not try a chicken madras?')
+    elif message.content == 'beef':
+        await message.channel.send('try and make a lasagne?' '\n' 'https://www.goodhousekeeping.com/uk/food/recipes/a544697/classic-lasagne-544697/')   #<--- Links user to a web to cook lasagne
+    elif message.content == 'lamb':
+        await message.channel.send('lamb is a tough one, why not try this:' '\n' '')
+
+    if message.content == 'show me a list of currys please':
+        await message.channel.send('here is a website for currys. I recommend the chicken Madras!' '\n' 'https://www.bbcgoodfood.com/recipes/collection/curry')
+    
+
+#APIKEY = 91e262c6007a70f8f52fe5c61dfcdb7d
+
+
+
+
+client.run('NjI4MzYyNTM4ODIzNzEyNzg4.XcWzhw.RS8AWHp0y28MAvj4E_A4rdv10iY')
